c7bf34c7361b6f7e9774194aa2b560e67336abed
Saumitra Tiwari <saumitra.tiwari.03@gmail.com>
2021-10-27 22:42:18 +0530

    First Commit

---
+++ src/main/java/com/saumitra/card/issuance/CardIssuanceApplication.java
@@ -0,0 +1,13 @@
+package com.saumitra.card.issuance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardIssuanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardIssuanceApplication.class, args);
+	}
+
+}
